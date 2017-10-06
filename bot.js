d60bab57d6b24f1616c2d9f24479a7159a1b72e3
matt <mattvalladarez@gmail.com>
2017-10-06 08:23:31 -0500

    adds basic

--- bot.js
+++ bot.js
@@ -1,6 +1,8 @@
+//Telling Node we're using twit and node-twitterbot
 var Twit = require('twit');
 var TwitterBot = require('node-twitterbot').TwitterBot;
 
+//use heroku config variables
 var Bot = new TwitterBot ({
     consumer_key: process.env.BOT_CONSUMER_KEY,
     consumer_secret: process.env.BOT_CONSUMER_SECRET,
@@ -8,12 +10,13 @@ var Bot = new TwitterBot ({
     access_token_secret: process.env.BOT_ACCESS_TOKEN_SECRET
 });
 
+//instantiating an array with a number of phrases
 var phraseArray = [
-                    "hey twitter",
-                    "im tweeting",
-                    "tweet tweet",
-                    "tweetstorm time... 1/22",
-                    "plz RT v important",
+                    "Tillerson said #trump is a moron",
+                    "#startrek discovery",
+                    "Lebron said #trump is a bum",
+                    "rand paul called #trump gollum",
+                    "Adam Sandler, David Spade, Chris Farley",
                     "delete ur account",
                     "it me",
                     "same",
@@ -21,6 +24,14 @@ var phraseArray = [
                     "#thedress is blue and black"
 ];
 
-function chooseRandom(phraseArray) + "," + chooseRandom(phraseArray);
+//function to choose random array 
+function chooseRandom(myArray) {
+    return myArray[Math.floor(Math.random() * myArray.length)];  
+}
 
+//saving array as variable
+var phrase = chooseRandom(phraseArray) + ", " + 
+chooseRandom(phraseArray);
+
+//node-twitterbot's bot.tweet function
 Bot.tweet(phrase);
